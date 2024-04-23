c02d41b989eeb790a0e28ac6375ec5ac74efb491
Kelldoor <145621342+Kelldoor@users.noreply.github.com>
2024-04-23 10:26:50 -0500

    included both API addresses

--- my_campus_marketplace_front_end/lib/Repositories/userClient.dart
+++ my_campus_marketplace_front_end/lib/Repositories/userClient.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 
 import 'package:mycampusmarketplace/Models/user.dart';
 
-const String apiAddress = "https://helpmewithfinals.com/api/";
+const String apiAddress = "http://10.0.2.2/api/";
+//const String apiAddress = "https://helpmewithfinals.com/api/";
 
 class UserClient {
   String sessionState = "";
