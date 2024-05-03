9b425d306a4627f14d0ff4743ad6c764a7eef113
Aaron Kingsley <118222446+kingsley-aaron@users.noreply.github.com>
2024-05-03 11:38:03 -0500

    AdminApp bar created and added, and fixed nav error from main menu to for sale

---
+++ my_campus_marketplace_front_end/lib/Views/adminAppBar.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:mycampusmarketplace/Views/loginview.dart';
+import 'package:mycampusmarketplace/Views/adminMain.dart';
+import 'package:mycampusmarketplace/theme.dart';
+import 'package:mycampusmarketplace/main.dart' as m;
+
+class CustomAdminAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String userName;
+
+  CustomAdminAppBar({
+    required this.userName,
+  });
+
+  void _logout(BuildContext context) async {
+    // Calling the logout function
+    String logoutResponse = await m.userClient.logout();
+
+    if (logoutResponse == "Success") {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginSignupPage()),
+      );
+    } else {
+      _showErrorDialog(context, logoutResponse);
+    }
+  }
+
+  void _showErrorDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Error"),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      actions: [
+        IconButton(
+          icon: Icon(Icons.home),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => AdminHome(userName: userName)),
+            );
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Row(
+            children: [
+              Text(
+                'Welcome, $userName',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              Container(
+                width: 120,
+                child: PopupMenuButton<String>(
+                  padding: EdgeInsets.zero,
+                  onSelected: (value) {
+                    if (value == 'signOut') {
+                      _logout(context); // Call logout function
+                    }
+                  },
+                  itemBuilder: (BuildContext context) =>
+                      <PopupMenuEntry<String>>[
+                    PopupMenuItem<String>(
+                      value: 'signOut',
+                      child: Text(
+                        'Sign Out',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+      // Apply the AppBar theme styles
+      backgroundColor: AppTheme.themeData.appBarTheme.backgroundColor,
+      elevation: AppTheme.themeData.appBarTheme.elevation,
+      centerTitle: AppTheme.themeData.appBarTheme.centerTitle,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
